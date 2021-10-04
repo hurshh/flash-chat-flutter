1aa46aff73a0479b0858565337a6dcb43c3951c0
hurshh <quasarhrsh@gmail.com>
2021-10-04 23:43:37 +0530

    added chat bubbles

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,6 +13,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+  final messageTextEditingController = TextEditingController();
   User loggedInUser ;
   Future<void> getCurrentUser() async {
     final user = await _auth.currentUser;
@@ -24,6 +25,20 @@ class _ChatScreenState extends State<ChatScreen> {
     }
     catch(e){
       print(e);
+    }
+  }
+
+  // Future<void> getMessages() async {
+  //   final messages =  await _fireStore.collection('messages').get();
+  //   for(var message in messages.docs){
+  //     print(message.data());
+  //   }
+  // }
+  void messagesStream() async{
+    await for(var snaps in _fireStore.collection('messages').snapshots()){
+     for(var messages in snaps.docs){
+       print(messages.data());
+     }
     }
   }
 
@@ -43,6 +58,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
+                // getMessages();
+                messagesStream();
                 //Implement logout functionality
               }),
         ],
@@ -54,6 +71,25 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(builder: (context,snapshot){
+              if(snapshot.hasData){
+                final messages = snapshot.data.docs;
+                List<messageBubble> messageWidgets = [];
+                for(var message in messages){
+                  final messageText = message.get('text');
+                  final messageSender = message.get('sender');
+                  final MessageBubble = messageBubble(sender: messageSender,text: messageText,);
+                  messageWidgets.add(MessageBubble);
+                }
+                return Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+                    children: messageWidgets,
+                  ),
+                );
+              }
+            },stream: _fireStore.collection('messages').snapshots(),
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -61,6 +97,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextEditingController,
                       onChanged: (value) {
                         //Do something with the user input.
                         message = value;
@@ -70,12 +107,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () async {
+                      messageTextEditingController.clear();
                       //Implement send functionality.
                       _fireStore.collection('messages').add({
                         'sender' : loggedInUser.email,
                         'text' : message,
                       });
-                      
+
                     },
                     child: Text(
                       'Send',
@@ -91,3 +129,33 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class messageBubble extends StatelessWidget {
+
+  final String sender,text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text('$sender',style: TextStyle(fontSize: 14,color: Colors.black45),),
+          Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.blueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+              child: Text(
+                '$text',style: TextStyle(fontSize: 20,color: Colors.white),
+              ),
+            )
+        ),],
+      ),
+    );
+  }
+
+  messageBubble({this.sender, this.text});
+}
+
